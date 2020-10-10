6cc1f2b48006464b3b3080dee2473523b79a002c
erik <erikvandeursen@gmail.com>
2020-10-10 20:48:11 +0200

    Formatter added in webpack.config.js

--- test/cypress/cypress/webpack.config.js
+++ test/cypress/cypress/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
               formatter: require("eslint/lib/formatters/stylish")
             }
           }
